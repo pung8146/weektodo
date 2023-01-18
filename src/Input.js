d50a895c40afedcc3dcf65e92be52ad1266a1a35
pung8146 <pung8146@naver.com>
2023-01-18 19:09:25 +0900

    기초 파일 뼈대 구성

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Input() {
+
+    const $input = '';
+
+  return (
+    <div>
+        <input></input>
+        <button>Submit</button>
+    </div>
+  )
+}
---
+++ src/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+export default function List() {
+  return (
+    <div className='content'>
+        <h2>text</h2>
+    </div>
+  )
+}
